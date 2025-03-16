b39487709d7e547baf3509800cde5dba0f4b0403
COMICP <binettejaden@gmail.com>
2025-03-15 21:03:55 -0400

    CRUD complete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,7 +24,75 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createCont = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .insertOne(contact);
+
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    console.log(response);
+    res
+      .status(500)
+      .json(response.error || "some error occured updating the contact.");
+  }
+};
+
+const updateCont = async (req, res) => {
+  const userID = new objectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: userID }, contact);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "some error occured updating the contact.");
+  }
+};
+
+const deleteCont = async (req, res) => {
+  const userID = new objectId(req.params.id);
+
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("contacts")
+    .deleteOne({ _id: userID });
+
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "some error occured updating the contact.");
+  }
+};
 module.exports = {
   getAll,
   getSingle,
+  updateCont,
+  createCont,
+  deleteCont,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = require("express").Router();
-const contactsController = require('../controllers/contacts')
+const contactsController = require("../controllers/contacts");
 
-router.get("/", contactsController.getAll)
+router.get("/", contactsController.getAll);
 
-router.get("/:id", contactsController.getSingle)
+router.get("/:id", contactsController.getSingle);
+
+router.post("/", contactsController.createCont);
+
+router.put("/:id", contactsController.updateCont);
+
+router.delete("/:id", contactsController.deleteCont);
 
 module.exports = router;
